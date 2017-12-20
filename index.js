8d4e3042804913ce4b57f4bba801d0ea6caf4a02
Xiaolong Tang <xxlongtang@gmail.com>
2017-12-20 10:46:54 -0500

    More exposed packages

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 define([
+    'jquery',
     'meld',
     'locache',
     'underscore',
+    'constraintjs',
     'backbone',
     'backbone-paginator'
-], function(
-    meld,
-    locache,
-    _, 
-    backbone) {
+], function($,
+            meld,
+            locache,
+            _,
+            cjs,
+            backbone) {
 
     'use strict';
 
     return {
+        'jquery': $,
         'meld': meld,
         'locache': locache,
         'underscore': _,
+        'constraintjs': cjs,
         'backbone': backbone
     };
 });
